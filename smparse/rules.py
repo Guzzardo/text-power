17e8f61bc3ebff97b46bf9fd5301fb07a74b3b8d
Guzzardo <guzzardo.joe@gmail.com>
2015-05-16 14:49:24 -0400

    other ratios

--- smparse/rules.py
+++ smparse/rules.py
@@ -1,6 +1,24 @@
 from collections import defaultdict
 from helpers import *
+import numpy
 from textblob import TextBlob
+
+"""
+poop emoji count✔︎
+sentiment analysis✔︎
+neediness
+	I want✔︎
+	I need✔︎
+	could you✔︎
+	will you✔︎
+sorry count✔︎
+big words count✔︎
+all caps (passion)✔︎
+texts least✔︎
+average text length✔︎
+k count✔︎
+"""
+
 
 # {:adam => {:pos => 42, :neg => 53, :neutral => 54}}
 def sentiment_count(text):
@@ -207,6 +225,31 @@ def contact_ratio_neediness(texts, message):
 			participants[text['name']] += text['message'].lower().count(swear)
 	return participants
 
+def contact_ratio_word_count(texts, message, keyword):
+	participants = defaultdict(int)
+	for text in texts:
+		if keyword in text['message'].lower():
+			participants[text['name']] += text['message'].lower().count(keyword)
+	return participants
+
+def contact_ratio_neediness_i_want(texts, message):
+	return contact_ratio_word_count(texts, message, 'i want')
+
+def contact_ratio_neediness_i_need(texts, message):
+	return contact_ratio_word_count(texts, message, 'i need')
+
+def contact_ratio_neediness_could_you(texts, message):
+	return contact_ratio_word_count(texts, message, 'could you')
+
+def contact_ratio_neediness_would_you(texts, message):
+	return contact_ratio_word_count(texts, message, 'would_you')
+
+def contact_ratio_sorry(texts, message):
+	return contact_ratio_word_count(texts, message, 'sorry')
+
+def contact_ratio_poop(texts, message):
+	return contact_ratio_word_count(texts, message, u'\udca9')
+
 def contact_ratio_all_caps(texts, message):
 	"""Returns how many times each party sent all caps"""
 	participants = defaultdict(int)
@@ -215,14 +258,23 @@ def contact_ratio_all_caps(texts, message):
 			participants[text['name']] += 1
 	return participants
 
-def contact_ratio_poop(texts, message):
-	"""Returns how many times each party sent poop emoji"""
+def contact_ratio_k(texts, message):
 	participants = defaultdict(int)
 	for text in texts:
-		if u'\udca9' in text['message']:
+		if text['message'].lower() == 'k':
 			participants[text['name']] += 1
 	return participants
 
+def contact_ratio_average_length(texts, message):
+	"""Returns how many times each party sent all caps"""
+	participants = defaultdict(list)
+	for text in texts:
+		participants[text['name']].append(len(text['message']))
+	return dict((name, "%.2f" % numpy.mean(sizes)) for name, sizes in participants.items())
 
-
-
+def contact_ratio_big_words(texts, message):
+	participants = defaultdict(int)
+	for text in texts:
+		if len(text['message']) >= 10:
+			participants[text['name']] += 1
+	return participants
