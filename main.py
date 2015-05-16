5f229f8c97a629b2d45f407b2a24bd2cd1acbe09
Guzzardo <guzzardo.joe@gmail.com>
2015-05-15 23:11:44 -0400

    test templates

--- main.py
+++ main.py
@@ -2,10 +2,18 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask
+from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return 'Hello from Flask!'
+    if request.method == 'POST':
+
+
+        return render_template('test_results.html', name='name')
+    else:
+        return render_template('test_home.html', name='name')
+
 
