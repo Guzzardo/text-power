19c88fc852e304d28fc8eaa809f9a1e8a13d20b2
Adam Tal <admtal@gmail.com>
2015-05-16 16:02:10 -0400

    More weby stuff

--- main.py
+++ main.py
@@ -5,14 +5,19 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
+from smparse.helpers import *
+from smparse.rules import *
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     if request.method == 'POST':
-        messages_content = request.files['messages'].read().decode('utf-16')
+        file_contents = request.files['messages'].read().decode('utf-16')
 
-        return render_template('test_results.html', messages_content=messages_content)
+        username, texts = parse_message_file(file_contents)
+
+        return render_template('test_results.html', messages_content=texts)
     else:
         return render_template('landing.html', name='name')
 
@@ -20,6 +25,7 @@ def hello_world():
 @app.route('/send_sms', methods=['GET'])
 def send_message():
 	send_text()
+	return ''
 
 
 if __name__ == '__main__':
